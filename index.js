3475855aaab97ca38165916d50ab374c7aa7924f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-11-30 19:10:26 +0900

    update devDependnecies
    Highlight: spaces -> tabs https://github.com/shinnn/eslint-config/commit/e92384d999f10fd0e477e6ada6b979c48977a2f7

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const loadFromCwdOrNpm = require('load-from-cwd-or-npm');
 const promise = loadFromCwdOrNpm('request');
 
 module.exports = function loadRequestFromCwdOrNpm() {
-  return promise;
+	return promise;
 };
