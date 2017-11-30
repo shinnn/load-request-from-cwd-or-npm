e961c266a65dd37a2bd4b563f022e20d5a130037
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-11-30 19:11:01 +0900

    unify test scripts into a single one

--- test/test.js
+++ test/test.js
@@ -1,31 +1,63 @@
 'use strict';
 
-const assert = require('assert');
-const http = require('http');
+const {createServer} = require('http');
+const {join, resolve} = require('path');
 
-const loadRequestFromCwdOrNpm = require('..');
 const test = require('tape');
+const clearAllModules = require('clear-module').all;
 
-test('loadRequestFromCwdOrNpm()', t => {
-  t.plan(2);
+test('loadRequestFromCwdOrNpm()', async t => {
+	t.plan(1);
 
-  t.strictEqual(loadRequestFromCwdOrNpm.name, 'loadRequestFromCwdOrNpm', 'should have a function name.');
+	const loadRequestFromCwdOrNpm = require('..');
 
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end('{"a": 1}');
-  }).listen(8124);
+	const server = createServer((req, res) => {
+		res.writeHead(200, {'Content-Type': ''});
+		res.end(Buffer.from('1'));
+	}).listen(8124);
 
-  loadRequestFromCwdOrNpm().then(request => {
-    request.get('http://localhost:8124', {json: true}, (err, res, body) => {
-      server.close();
-      assert.ifError(err);
+	const request = await loadRequestFromCwdOrNpm();
+	request.get('http://localhost:8124', {encoding: 'utf8'})
+	.on('data', data => t.equal(data, '1', 'should load a valid `request` module.'))
+	.on('end', () => server.close());
+});
 
-      t.deepEqual(
-        body,
-        {a: 1},
-        'should load a valid `request` module.'
-      );
-    });
-  }).catch(t.fail);
+test('loadRequestFromCwdOrNpm() with broken npm CLI', async t => {
+	t.plan(1);
+
+	clearAllModules();
+
+	process.env.PATH = join(__dirname, 'fixtures');
+	const loadRequestFromCwdOrNpm = require('..');
+
+	try {
+		await loadRequestFromCwdOrNpm();
+	} catch ({message}) {
+		t.equal(
+			message.includes('Install "request" and try again. (`npm install request`)'),
+			true,
+			'should fail when npm CLI is not installed correctly.'
+		);
+	}
+});
+
+test('loadRequestFromCwdOrNpm() with no npm CLI', async t => {
+	t.plan(1);
+
+	clearAllModules();
+
+	process.env.PATH = resolve('n/p/m/_/d/o/e/s/_/n/o/t/_/e/x/i/s/t/_/i/n/_/t/h/i/s/_p/a/t/h');
+	const loadRequestFromCwdOrNpm = require('..');
+
+	try {
+		await loadRequestFromCwdOrNpm();
+	} catch ({message}) {
+		t.equal(
+			message,
+			`Failed to load "request" module from the current working directory (${
+				process.cwd()
+			}). Install "request" and try again. (\`npm install request\`)`,
+			'should fail when npm CLI is not installed.'
+		);
+	}
 });
